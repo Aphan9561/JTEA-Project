8fe2b759cd2ce31b3755c3b33f177ed19679f52e
Aphan9561 <phanville36039@gmail.com>
2023-03-14 12:48:27 -0400

    add UserList

---
+++ UserList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class UserList {
+    private ArrayList<User> User;
+    private static UserList userList;
+
+    private UserList()
+    {
+
+    }
+    public UserList getInstance(){
+        return null;
+    }
+
+    public void addUser(String firstName, String lastName, String email, String birthday)
+    {
+
+    }
+
+    public User getUser(String UserName){
+        return null;
+    }
+
+    public void editUser()
+    {
+
+    }
+
+    public void saveUser(){
+        
+    }
+}
