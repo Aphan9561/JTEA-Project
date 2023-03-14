8649af89f5adde0d373c97f58b72ff39f03fc856
Tessa Neal <tessan@email.sc.edu>
2023-03-14 12:24:43 -0400

    Adding question & quiz class stubs

---
+++ Question.java
@@ -0,0 +1,11 @@
+public class Question {
+    
+    public static final int maxAnswers = 4;
+    public String question;
+    public String[] answers = new String[maxAnswers];
+    public int correctAnswer;
+
+    public Question(String question, String[] answers, int correctAnswer) {
+
+    }
+}
---
+++ Quiz.java
@@ -0,0 +1,21 @@
+public class Quiz {
+    public static final int quizSize = 10;
+    private Question[] questions = new Question[quizSize];
+    private int[] grades = new int[quizSize];
+
+    public Quiz(Question[] questions) {
+
+    }
+
+    public void addQuestion(Question question) {
+
+    }
+
+    public double calculateScore() {
+        return 0;
+    }
+
+    public void giveStars() {
+
+    }
+}
