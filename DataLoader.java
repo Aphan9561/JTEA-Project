12c1889fe627f4281b32e9cc66985ebd1d6ca638
Tessa Neal <tessan@email.sc.edu>
2023-03-14 12:56:36 -0400

    starting data loader class

--- DataLoader.java
+++ DataLoader.java
@@ -1,9 +1,29 @@
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.UUID;
 
-public class DataLoader {
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class DataLoader extends DataConstants{
     
     public ArrayList<User> getUsers() {
+        ArrayList<User> users = new ArrayList<User>();
 
+        try {
+            FileReader reader = new FileReader(./JSON/course.json);
+            JSONParser parser = new JSONParser();
+            JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+
+            for(int i=0; i < peopleJSON.size(); i++) {
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public ArrayList<Course> getCourses() {
@@ -11,6 +31,6 @@ public class DataLoader {
     }
 
     public ArrayList<FAQ> getFAQs() {
-        
+
     }
 }
