264d5900521c569a1384e46d2206a1cb09d41a4b
Aphan9561 <phanville36039@gmail.com>
2023-03-14 22:05:21 -0400

    Adding certification

---
+++ Certification.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Certification {
+    private String firstName;
+    private String lastName;
+    private String courseName;
+    private Date dateCompleted;
+    
+    public Certification(String firstName, String lastName, String courseName, Date dateCompleted){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.courseName = courseName;
+        this.dateCompleted = dateCompleted;
+    }
+}
