94d7b4537eb900d4a060725d2126bb29f868bf4a
Aphan9561 <phanville36039@gmail.com>
2023-03-15 10:44:22 -0400

    updating author

--- Author.java
+++ Author.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.UUID;
 
 public class Author extends User{
     private ArrayList<Course> myCourses;
 
-    public Author(UUID id,String firstName, String lastName, String email, String birthday,String username)
+    public Author(UUID id,String firstName, String lastName, String email, Date birthday,String username, String type)
     {
-        super(id, firstName, lastName,email,birthday,username);
+        super(id, firstName, lastName, email, birthday, username, type);
     }
 
     public void addCourse(String title, String decription, String syllabus, Difficulty difficult, Language language){
