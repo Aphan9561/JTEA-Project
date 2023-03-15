d6cfacd11d6ca521392592177cff273815c55f1a
Tessa Neal <tessan@email.sc.edu>
2023-03-15 08:15:22 -0400

    Adding FAQ classes

---
+++ FAQ.java
@@ -0,0 +1,16 @@
+public class FAQ {
+    
+    private Question question;
+
+    public FAQ(String question) {
+
+    }
+
+    public FAQ(Question question) {
+
+    }
+
+    public void answerQuestion(String answer) {
+        
+    }
+}
---
+++ FAQList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class FAQList {
+    
+    private ArrayList<FAQ> FAQs;
+    private FAQList FAQList;
+
+    public FAQList() {
+        
+    }
+    
+    public FAQList getInstance() {
+        return null;
+    }
+
+    public void addFAQ(Question question) {
+
+    }
+
+    public void addFAQ(String question) {
+
+    }
+}
