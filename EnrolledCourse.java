16d95e23ea312f0714ea8778fb5077d1cdb07135
Aphan9561 <phanville36039@gmail.com>
2023-03-30 16:05:54 -0400

    updating

--- EnrolledCourse.java
+++ EnrolledCourse.java
@@ -1,102 +1,90 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
-/**
- * An Enrolled Course
- * @author: J TEA: Tessa Neal, Eve Blom, Anna Phan, and Jacqueline Askey
- */
 public class EnrolledCourse {
-    private boolean enrolled;
+    private UUID course;
+    //private boolean enrolled;
     private Progress progress;
-    private int progressPercentage;
+    //private int progressPercentage;
     private int currentModule;
     private int currentLesson;
-    // private Grade grade;
-    // private ArrayList<Grade> gradePerModule;
+    private int overallGrade;
+    private ArrayList<Integer> gradesPerModule = new ArrayList<Integer>();
 
-    /**
-     * Creates an Enrolled Course
-     * @param course a course
-     * @param enrolled if user is enrolled in the course or not
-     * @param progress the course progress
-     */
-    public EnrolledCourse(Course course, boolean enrolled, Progress progress){
+    
+    public EnrolledCourse(UUID course, Progress progress, int currentModule, int currentLesson, int overallGrade, ArrayList<Integer> gradesPerModule){
         
-        this.enrolled = enrolled;
+        this.course = course;
         this.progress = progress;
+        //this.progressPercentage = progressPercentage;
+        this.currentModule = currentModule;
+        this.currentLesson = currentLesson;
+        //this.enrolled = enrolled;
+        this.overallGrade = overallGrade;
+        gradesPerModule.addAll(gradesPerModule);
     }
 
+    public EnrolledCourse(UUID course){
+        this.course = course;
+        this.currentModule = 0;
+        this.currentLesson = 0;
+        //this.enrolled = enrolled;
+    }
 
-    /**
-     * Returns the progress percentage
-     * @return the progress percentage
-     */
+    public UUID getCourse() {
+        return course;
+    }
+
+    /*
     public int getProgressPercentage(){
         return progressPercentage;
     }
+    */
 
-    /**
-     * Returns progress of the Course
-     * @return progress of the Course
-     */
     public Progress getProgress(){
         return progress;
     }
 
-    /**
-     * Returns the current lesson
-     * @return the current lesson
-     */
     public int getCurrentLesson(){
         return currentLesson;
     }
 
-    /**
-     * Returns the Module last worked on
-     * @return a course
-     */
+    /*
     public Module resumeModule(){
         return course.getModule(currentModule);
     }
 
-    /**
-     * Returns the Lesson last worked on
-     * @return a lesson
-     */
     public Lesson resumeLesson(){
         Module module = course.getModule(currentModule);
         return module.getLesson(currentLesson);
     }
+    */
 
-    /**
-     * Returns the current module
-     * @return the current module
-     */
     public int getCurrentModule() {
         return currentModule;
     }
 
-    /**
-     * Returns a course
-     * @return a course
-     */
+    public int getOverallGrade() {
+        return overallGrade;
+    }
+
+    public ArrayList<Integer> getGradesPerModule() {
+        return gradesPerModule;
+    }
+
+    /*
     public Course getCourse(){
         return this.course;
     }
+    */
 
-    /**
-     * Changes the index of the current module
-     * @return the current module index
-     */
     public int moveCurrentModule(){
         currentModule++;
         return currentModule;
     }
 
-    /**
-     * Changes the index of the current lesson
-     * @return the current lesson index
-     */
     public int moveCurrentLesson(){
+        //if()
         currentLesson++;
         return currentLesson;
     }
