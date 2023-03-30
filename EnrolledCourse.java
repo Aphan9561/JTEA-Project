ca6a6a6d8f322b6537d50b3813b030b19b15d876
Jacqueline Askey <jaskey@email.sc.edu>
2023-03-30 16:18:53 -0400

    java docing

--- EnrolledCourse.java
+++ EnrolledCourse.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * An Author User
+ * @authors: J TEA: Tessa Neal, Eve Blom, Anna Phan, and Jacqueline Askey
+ */
 public class EnrolledCourse {
     private UUID course;
     //private boolean enrolled;
@@ -11,7 +15,15 @@ public class EnrolledCourse {
     private int overallGrade;
     private ArrayList<Integer> gradesPerModule = new ArrayList<Integer>();
 
-    
+    /**
+     * Creates a Enrolled Course
+     * @param course
+     * @param progress
+     * @param currentModule
+     * @param currentLesson
+     * @param overallGrade
+     * @param gradesPerModule
+     */
     public EnrolledCourse(UUID course, Progress progress, int currentModule, int currentLesson, int overallGrade, ArrayList<Integer> gradesPerModule){
         
         this.course = course;
@@ -24,6 +36,10 @@ public class EnrolledCourse {
         gradesPerModule.addAll(gradesPerModule);
     }
 
+    /**
+     * Creates an Enrolled Course
+     * @param course a UUID
+     */
     public EnrolledCourse(UUID course){
         this.course = course;
         this.currentModule = 0;
@@ -31,6 +47,10 @@ public class EnrolledCourse {
         //this.enrolled = enrolled;
     }
 
+    /**
+     * Returns course id
+     * @return course id
+     */
     public UUID getCourse() {
         return course;
     }
@@ -41,10 +61,18 @@ public class EnrolledCourse {
     }
     */
 
+    /**
+     * Returns progress of the Course
+     * @return progress of the Course
+     */
     public Progress getProgress(){
         return progress;
     }
 
+    /**
+     * Returns the current lesson
+     * @return the current lesson
+     */
     public int getCurrentLesson(){
         return currentLesson;
     }
@@ -60,14 +88,26 @@ public class EnrolledCourse {
     }
     */
 
+    /**
+     * Returns the current module
+     * @return the current module
+     */
     public int getCurrentModule() {
         return currentModule;
     }
 
+    /**
+     * Returns the over all grade
+     * @return the over all grade
+     */
     public int getOverallGrade() {
         return overallGrade;
     }
 
+    /**
+     * Returns an Array List of grades per module
+     * @return an Array List of grades per module
+     */
     public ArrayList<Integer> getGradesPerModule() {
         return gradesPerModule;
     }
@@ -78,11 +118,19 @@ public class EnrolledCourse {
     }
     */
 
+    /**
+     * Changes the index of the current module
+     * @return the current module index
+     */
     public int moveCurrentModule(){
         currentModule++;
         return currentModule;
     }
 
+    /**
+     * Changes the index of the current lesson
+     * @return the current lesson index
+     */
     public int moveCurrentLesson(){
         //if()
         currentLesson++;
